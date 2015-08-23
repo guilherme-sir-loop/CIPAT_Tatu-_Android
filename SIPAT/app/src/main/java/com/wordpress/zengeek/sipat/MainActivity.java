a9ec4a2e0d9b4d764c0593bc0b85d1db407eea46
guilherme-sir-loop <guilhermeaugustopc@gmail.com>
2015-08-23 19:48:04 -0300

    Alteração SIPAT

--- SIPAT/app/src/main/java/com/wordpress/zengeek/sipat/MainActivity.java
+++ SIPAT/app/src/main/java/com/wordpress/zengeek/sipat/MainActivity.java
@@ -4,13 +4,42 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TabHost;
 
 public class MainActivity extends AppCompatActivity {
+    TabHost th;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        th = (TabHost)findViewById(R.id.th);
+
+        //Tab1 with a list of Talks(Palestras in Portuguese)
+        th.setup();
+        TabHost.TabSpec ts1 = th.newTabSpec("Tab1");
+        ts1.setIndicator("Palestras");
+        ts1.setContent(R.id.tab1);
+        th.addTab(ts1);
+
+        //Tab2 about SIPAT(it is event which have the focus of prevent accidents)
+        th.setup();
+        TabHost.TabSpec ts2 = th.newTabSpec("Tab2");
+        ts2.setIndicator("Sobre");
+        ts2.setContent(R.id.tab2);
+        th.addTab(ts2);
+
+        //Tab2 about SIPAT(it is event which have the focus of prevent accidents)
+        th.setup();
+        TabHost.TabSpec ts3 = th.newTabSpec("Tab2");
+        ts3.setIndicator("Créditos");
+        ts3.setContent(R.id.tab3);
+        th.addTab(ts3);
+
+
+
+
+
     }
 
     @Override
