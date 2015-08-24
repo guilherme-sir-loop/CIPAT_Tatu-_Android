847f091c57a37c66b8b5794f9ce1e46368de8ff5
guilherme-sir-loop <guilhermeaugustopc@gmail.com>
2015-08-24 20:11:24 -0300

    Adicionando o botão

--- SIPAT/app/src/main/java/com/wordpress/zengeek/sipat/MainActivity.java
+++ SIPAT/app/src/main/java/com/wordpress/zengeek/sipat/MainActivity.java
@@ -1,19 +1,69 @@
 package com.wordpress.zengeek.sipat;
 
+import android.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.TabHost;
+import android.widget.*;
+import android.view.*;
 
 public class MainActivity extends AppCompatActivity {
-    TabHost th;
+    private TabHost th;
+    private Button btnMorning;
+    private Button btnEvening;
+    private Button btnNight;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         th = (TabHost)findViewById(R.id.th);
+        btnMorning = (Button)findViewById(R.id.btnMorning);
+        btnEvening = (Button)findViewById(R.id.btnEvening);
+        btnNight = (Button)findViewById(R.id.btnNight);
+
+        btnMorning.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder dlg = new AlertDialog.Builder(MainActivity.this);
+                dlg.setMessage("Segunda\nTipo:Palestra\nTema:Dengue\nHorário:10h\nPalestrante:Tony\n\n"+
+                               "Terça\nTipo:Palestra\nTema:Fisoterapia/LER\nHorário:10h\nPalestrante:Silva\n\n"+
+                               "Quarta\nTipo:Atividade\nTema:Fono\nHorário:9-12h\nPalestrante:Gislene\n\n"+
+                               "Quinta\nTipo:Atividade\nTema:Odontologia\nHorário:9-12h\nPalestrante:Leandro");
+                dlg.setNeutralButton("Ok",null);
+                dlg.show();
+
+
+            }
+        });
+        btnEvening.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder dlg = new AlertDialog.Builder(MainActivity.this);
+                dlg.setMessage("Segunda\nTipo:Atividade\nTema:Fono\nHorário:14-16h\nPalestrante:Gislaine\n\n"+
+                        "Terça\nTipo:Palestra\nTema:DENARC(conscientização sobre o uso indevido de narcóticos)\nHorário:16-17h\nPalestrante:Marcio Rodrigues\n\n"+
+                        "Quarta\nTipo:Palestra\nTema:DST/AIDS/HIV/Sifilis\nHorário:14-15h\nPalestrante:Marilu\n\n"+
+                        "Quinta\nTipo:Palestra\nTema:Orientação sobre sexualidade/mama/próstata\n Horário:14h\nPalestrante:Karina");
+                dlg.setNeutralButton("Ok",null);
+                dlg.show();
+
+            }
+        });
+        btnNight.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder dlg = new AlertDialog.Builder(MainActivity.this);
+                dlg.setMessage("Segunda\nTipo:Palestra\nTema:Segurança do Trabalho\nHorário:19h\nPalestrante:Prof. Otávio\n\n"+
+                        "Terça\nTipo:Palestra\nTema:Direção Defensiva\nHorário:19h\nPalestrante:Sr. Carlos Eduardo");
+                dlg.setNeutralButton("Ok",null);
+                dlg.show();
+
+            }
+        });
+
+
+
 
         //Tab1 with a list of Talks(Palestras in Portuguese)
         th.setup();
